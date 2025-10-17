966d63a0216e7c87bb568365a903f0036f05fc20
JadeRowan <rougesoul.m@gmail.com>
2025-10-17 15:21:07 +0300

    Remove saving chatNames in session

--- src/main/java/com/websocket/chat/websocket/config/WebSocketEventListener.java
+++ src/main/java/com/websocket/chat/websocket/config/WebSocketEventListener.java
@@ -2,9 +2,13 @@ package com.websocket.chat.websocket.config;
 
 import static com.websocket.chat.websocket.config.WebSocketConfig.TOPIC;
 
+import com.websocket.chat.chat.domain.DomainChat;
+import com.websocket.chat.chat.service.ChatService;
 import com.websocket.chat.message.ChatMessage;
 import com.websocket.chat.message.MessageType;
 import com.websocket.chat.message.service.MessageService;
+import com.websocket.chat.user.service.UserService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
@@ -20,29 +24,31 @@ public class WebSocketEventListener {
 
     private final SimpMessageSendingOperations messageTemplate;
     private final MessageService messageService;
+    private final UserService userService;
+    private final ChatService chatService;
 
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String username = (String) headerAccessor.getSessionAttributes().get("username");
 
-        if (username == null) {
-            log.error("Username must not be null");
+        if (username == null && !userService.exists(username)) {
+            log.error("Invalid username during SessionDisconnectEvent processing");
             return;
         }
-        String chats = (String) headerAccessor.getSessionAttributes().get("chats");
-        if (chats != null) {
-            for (String room : chats.split(":")) {
-                log.info("User disconnected: {}", username);
-                var chatMessage = ChatMessage.builder()
-                        .type(MessageType.LEAVE)
-                        .sender(username)
-                        .room(room)
-                        .build();
-                String destinationUrl = TOPIC + "/" + room;
-                messageTemplate.convertAndSend(destinationUrl, chatMessage);
-                messageService.persist(chatMessage);
-            }
-        }
+        List<DomainChat> chats = chatService.getChatsByUserName(username);
+        chats.forEach(chat -> sendUserDisconnectedMessage(chat, username));
+    }
+
+    private void sendUserDisconnectedMessage(DomainChat chat, String username) {
+        log.info("User disconnected: {}", username);
+        var chatMessage = ChatMessage.builder()
+                .type(MessageType.LEAVE)
+                .sender(username)
+                .room(chat.getName())
+                .build();
+        String destinationUrl = TOPIC + "/" + chat.getName();
+        messageTemplate.convertAndSend(destinationUrl, chatMessage);
+        messageService.persist(chatMessage);
     }
 }
