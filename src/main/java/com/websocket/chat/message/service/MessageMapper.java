34473fabeae6ad12cdf4ce9bf7b9c9c6fba0cb75
JadeRowan <rougesoul.m@gmail.com>
2025-10-17 15:01:36 +0300

    Added GHA and demo tests

---
+++ src/main/java/com/websocket/chat/message/service/MessageMapper.java
@@ -0,0 +1,31 @@
+package com.websocket.chat.message.service;
+
+import com.websocket.chat.chat.domain.DomainChat;
+import com.websocket.chat.message.ChatMessage;
+import com.websocket.chat.message.domain.DomainMessage;
+import com.websocket.chat.user.domain.DomainUser;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageMapper {
+
+    public DomainMessage toDomainMessage(ChatMessage chatMessage, DomainUser user, DomainChat chat) {
+        return DomainMessage.builder()
+                .content(chatMessage.getContent())
+                .sender(user)
+                .room(chat)
+                .creationTime(chatMessage.getCreationTime())
+                .type(chatMessage.getType())
+                .build();
+    }
+
+    public ChatMessage toChatMessage(DomainMessage domainMessage) {
+        return ChatMessage.builder()
+                .content(domainMessage.getContent())
+                .sender(domainMessage.getSender().getName())
+                .room(domainMessage.getRoom().getName())
+                .creationTime(domainMessage.getCreationTime())
+                .type(domainMessage.getType())
+                .build();
+    }
+}
--- src/main/java/com/websocket/chat/message/service/MessageService.java
+++ src/main/java/com/websocket/chat/message/service/MessageService.java
@@ -21,43 +21,26 @@ public class MessageService {
     private final MessageRepository messageRepository;
     private final UserRepository userRepository;
     private final ChatRepository chatRepository;
-
+    private final MessageMapper mapper;
     @Transactional
     public ChatMessage persist(ChatMessage chatMessage) {
+
         Optional<DomainUser> user = userRepository.findByName(chatMessage.getSender());
         Optional<DomainChat> chat = chatRepository.findByName(chatMessage.getRoom());
         if (user.isEmpty() || chat.isEmpty()) {
             throw new IllegalArgumentException(
                     String.format("Can't persist message for user %s and chat %s", user, chat));
         }
-        DomainMessage newMessage = map(chatMessage, user.get(), chat.get());
-        return map(messageRepository.save(newMessage));
+        DomainMessage newMessage = mapper.toDomainMessage(chatMessage, user.get(), chat.get());
+        return mapper.toChatMessage(messageRepository.save(newMessage));
     }
 
     public List<ChatMessage> getAllByRoomChronologically(String room) {
         return messageRepository.findAllByRoomNameOrderByCreationTime(room).stream()
-                .map(MessageService::map)
+                .map(mapper::toChatMessage)
                 .collect(Collectors.toList());
     }
 
-    private static DomainMessage map(ChatMessage chatMessage, DomainUser user, DomainChat chat) {
-        return DomainMessage.builder()
-                .content(chatMessage.getContent())
-                .sender(user)
-                .room(chat)
-                .creationTime(chatMessage.getCreationTime())
-                .type(chatMessage.getType())
-                .build();
-    }
 
-    private static ChatMessage map(DomainMessage domainMessage) {
-        return ChatMessage.builder()
-                .content(domainMessage.getContent())
-                .sender(domainMessage.getSender().getName())
-                .room(domainMessage.getRoom().getName())
-                .creationTime(domainMessage.getCreationTime())
-                .type(domainMessage.getType())
-                .build();
-    }
 
 }
